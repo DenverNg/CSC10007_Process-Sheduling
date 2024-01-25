39faf8e3d600615f44e198d9076bee5fb068368c
Duy Nguyen <nhdduy67@gmail.com>
2024-01-25 23:49:35 +0700

    Fix bug by using // :clown:

--- 22127007_22127085-Code/function.cpp
+++ 22127007_22127085-Code/function.cpp
@@ -86,41 +86,7 @@ void Scheduler::executeScheduling()
 
 void Scheduler::fcfsScheduling()
 {
-    // Sort processes based on arrival time
-    sort(processes.begin(), processes.end(), [](const Process &a, const Process &b)
-         { return a.arrivalTime < b.arrivalTime; });
-
-    // Initialize Gantt chart for CPU and resource scheduling
-    int currentTime = 0;
-
-    for (const Process &process : processes)
-    {
-        // Update CPU schedule
-        for (int i = 0; i < process.cpuBurstTime; ++i)
-        {
-            cpuSchedule.push_back(process.arrivalTime);
-        }
-
-        // Update Resource schedule
-        for (int i = 0; i < process.resourceUsageTime; ++i)
-        {
-            resourceSchedule.push_back(process.arrivalTime);
-        }
-
-        // Update current time
-        currentTime += process.cpuBurstTime;
-
-        // Add waiting time for the next process if there is a gap
-        if (currentTime < process.arrivalTime)
-        {
-            for (int i = 0; i < process.arrivalTime - currentTime; ++i)
-            {
-                cpuSchedule.push_back(0); // 0 denotes idle time
-                resourceSchedule.push_back(0);
-            }
-            currentTime = process.arrivalTime;
-        }
-    }
+    // Implement FCFS scheduling logic here
 }
 
 void Scheduler::roundRobinScheduling()
@@ -147,52 +113,52 @@ void Scheduler::writeOutput(const string &outputFile)
         exit(EXIT_FAILURE);
     }
 
-    // Print Gantt chart for CPU scheduling to the output file
-    for (int i : cpuSchedule)
-    {
-        outfile << i << " ";
-    }
-    outfile << "\n";
+    // // Print Gantt chart for CPU scheduling to the output file
+    // for (int i : cpuSchedule)
+    // {
+    //     outfile << i << " ";
+    // }
+    // outfile << "\n";
 
-    // Print Gantt chart for resource scheduling to the output file
-    for (int i : resourceSchedule)
-    {
-        outfile << i << " ";
-    }
-    outfile << "\n";
+    // // Print Gantt chart for resource scheduling to the output file
+    // for (int i : resourceSchedule)
+    // {
+    //     outfile << i << " ";
+    // }
+    // outfile << "\n";
 
-    // Calculate turn-around time and waiting time
-    int totalTurnAroundTime = 0;
-    int totalWaitingTime = 0;
-    int currentTime = 0;
+    // // Calculate turn-around time and waiting time
+    // int totalTurnAroundTime = 0;
+    // int totalWaitingTime = 0;
+    // int currentTime = 0;
 
-    for (const Process &process : processes)
-    {
-        int turnAroundTime = currentTime - process.arrivalTime;
-        int waitingTime = turnAroundTime - process.cpuBurstTime;
-        totalTurnAroundTime += turnAroundTime;
-        totalWaitingTime += waitingTime;
+    // for (const Process &process : processes)
+    // {
+    //     int turnAroundTime = currentTime - process.arrivalTime;
+    //     int waitingTime = turnAroundTime - process.cpuBurstTime;
+    //     totalTurnAroundTime += turnAroundTime;
+    //     totalWaitingTime += waitingTime;
 
-        // Move the current time to the end of the current process
-        currentTime += process.cpuBurstTime;
-    }
+    //     // Move the current time to the end of the current process
+    //     currentTime += process.cpuBurstTime;
+    // }
 
-    // Print turn-around time for each process to the output file
-    for (const Process &process : processes)
-    {
-        int turnAroundTime = currentTime - process.arrivalTime;
-        outfile << turnAroundTime << " ";
-    }
-    outfile << "\n";
+    // // Print turn-around time for each process to the output file
+    // for (const Process &process : processes)
+    // {
+    //     int turnAroundTime = currentTime - process.arrivalTime;
+    //     outfile << turnAroundTime << " ";
+    // }
+    // outfile << "\n";
 
-    // Print waiting time for each process to the output file
-    for (const Process &process : processes)
-    {
-        int turnAroundTime = currentTime - process.arrivalTime;
-        int waitingTime = turnAroundTime - process.cpuBurstTime;
-        outfile << waitingTime << " ";
-    }
-    outfile << "\n";
+    // // Print waiting time for each process to the output file
+    // for (const Process &process : processes)
+    // {
+    //     int turnAroundTime = currentTime - process.arrivalTime;
+    //     int waitingTime = turnAroundTime - process.cpuBurstTime;
+    //     outfile << waitingTime << " ";
+    // }
+    // outfile << "\n";
 
-    outfile.close();
+    // outfile.close();
 }
